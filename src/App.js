f2ac9732b9fe229f000b5600bfc61b925450ad54
Riley Bergamasco <rlbergamasco@gmail.com>
2021-09-29 00:54:44 -0400

    Added TypedText and jsconfig

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
+import { React } from 'react';
+import { Typography, Box } from '@mui/material';
+import { TypedText } from 'components';
 function App() {
+  const realityTypes = ["Virtual", "Augmented", "Mixed"];
   return (
-    <h1>Hello world</h1>
+    <Box sx={{ height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+      <Typography variant="h1" align="center">What is <br></br>
+        <TypedText content={realityTypes} /> <br></br>
+        Reality?</Typography>
+    </Box >
   );
 }
 
---
+++ src/components/TypedText.jsx
@@ -0,0 +1,26 @@
+import { useRef, useEffect } from 'react';
+import Typed from 'typed.js';
+
+export const TypedText = ({content}) => {
+	const el = useRef(null);
+	const typed = useRef(null);
+
+  useEffect(() => {
+    const options = {
+    	strings: content,
+      typeSpeed: 100,
+      backSpeed: 50,
+      loop:true,
+      loopCount:Infinity
+    };
+    
+    typed.current = new Typed(el.current, options);
+    
+    return () => {
+      typed.current.destroy();
+    }
+  }, [content])
+  return (
+    <span ref={el} />
+    );
+}
