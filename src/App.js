9dcccb55884ab17b52d2d70ca72b18200d0c7b00
Riley Bergamasco <rlbergamasco@gmail.com>
2021-10-01 16:52:09 -0400

    Added snap scroll functionality

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import { React } from 'react';
+import './app.css';
 import { Typography, Box } from '@mui/material';
 import { TypedText } from 'components';
-function App() {
-  const realityTypes = ["Virtual", "Augmented", "Mixed"];
+import { Waypoint } from 'react-waypoint';
+import { StickyContainer, Sticky } from 'react-sticky';
+
+const App = () => {
+  const realityTypes = ["VIRTUAL", "AUGMENTED", "MIXED"];
+  const contents = [
+    <Typography variant="h1" align="center">What is <br></br>
+      <TypedText content={realityTypes} /> <br></br>
+      Reality?</Typography>,
+    <Typography variant="h2" align="center">Imagine you're in a living room.</Typography>,
+    <Typography variant="h2" align="center">You put on a virtual reality headset and now you're at the beach.</Typography>,
+    <Typography variant="h2" align="center">Virtual reality ...</Typography>,
+
+
+  ];
   return (
-    <Box sx={{ height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-      <Typography variant="h1" align="center">What is <br></br>
-        <TypedText content={realityTypes} /> <br></br>
-        Reality?</Typography>
-    </Box >
+    <div className="snap">
+      <div className="wrapper">
+        {contents.map((content, i) => (
+          <Box key={i} className="page" sx={{ height: '100vh', maxWidth: '1000px', margin: 'auto', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+            {content}
+          </Box >
+        ))}
+      </div>
+    </div>
   );
 }
 
