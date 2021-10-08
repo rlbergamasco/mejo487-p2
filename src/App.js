e2e8120b2bb2caab4618a751d045712bff7412ac
Riley Bergamasco <rlbergamasco@gmail.com>
2021-10-08 15:58:45 -0400

    Changed rotate message

--- src/App.js
+++ src/App.js
@@ -19,8 +19,44 @@ const App = () => {
   return (
     <React.Fragment>
       {/* if device is portrait, display message */}
-      <Typography className="rotate" variant="h2" align="center">Please rotate your device.</Typography>
-
+      <Box className="rotate">
+        <Box
+          className="under-3 absolute"
+          sx={{
+            height: "100vh",
+            margin: "auto",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <img src="images/livingroom.jpg" alt="living room" />
+        </Box>
+        <Box sx={{ height: '100vh', width: '100vw', display: 'flex', justifyContent: "center", alignItems: "center" }}>
+          <Grid>
+            <Typography className="gray-box" variant="h1" align="center">What is <br></br>
+              <TypedText content={realityTypes} /> <br></br>
+              Reality?</Typography>
+            <Box
+              sx={{ display: "flex", justifyContent: "center", alignItems: "flex-end" }}
+            >
+              <Box
+                sx={{
+                  backgroundColor: "rgba(255, 255, 255, 0.8)",
+                  display: "flex",
+                  alignContent: "center",
+                  justifyContent: "center",
+                  p: 1,
+                  m: 2,
+                  borderRadius: "10px",
+                }}
+              >
+                <Typography variant="button">Please rotate your device.</Typography>
+              </Box>
+            </Box>
+          </Grid>
+        </Box>
+      </Box>
       <div className="snap">
         <section>
 
